6f89607b66d7abff96546ccdf5fcc8f3cba563ff
Cole Thorsen <colethorsen@Coles-MacBook-Pro.local>
2019-01-15 11:11:55 -0500

    new icons

---
+++ build/icons.sql
@@ -0,0 +1,355 @@
+INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Airport', 'airport', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Autumn', 'autumn', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Baseball', 'baseball', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Basketball', 'basketball', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Beach Ball', 'beach-ball', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Beach Umbrella', 'beach-umbrella', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Bicycle', 'bicycle', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Bus', 'bus', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Cargo Truck', 'cargo-truck', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Church', 'church', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('City Hall', 'city-hall', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Construction', 'construction', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Cycling', 'cycling', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Default Notification', 'default-notification', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Factory', 'factory', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Ferris Wheel', 'ferris-wheel', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Field', 'field', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Fire', 'fire', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Flood', 'flood', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Football', 'football', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Frisbee', 'frisbee', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Gas Industry', 'gas-industry', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Gavel', 'gavel', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Golf', 'golf', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Graduation Cap', 'graduation-cap', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Hiking', 'hiking', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Hockey', 'hockey', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Hospital', 'hospital', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Monument', 'monument', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Oil Drum', 'oil-drum', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Playground', 'playground', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Police Badge', 'police-badge', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Restaurant', 'restaurant', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Roadblock', 'roadblock', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Running', 'running', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Sail Boat', 'sail-boat', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Ship', 'ship', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Skateboarding', 'skateboarding', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Soccer Ball', 'soccer-ball', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Stadium', 'stadium', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Stroller', 'stroller', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Summer', 'summer', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Swimming', 'swimming', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Taxi', 'taxi', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Tennis', 'tennis', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Tent', 'tent', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Tornado', 'tornado', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Tractor', 'tractor', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Train', 'train', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Umbrella', 'umbrella', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Volleyball', 'volleyball', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Water Park', 'water-park', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Weather Hail', 'weather-hail', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Weather Lightning', 'weather-lightning', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Weather Rain', 'weather-rain', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Weather Snow', 'weather-snow', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Wheat', 'wheat', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Windsurfing', 'windsurfing', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);INSERT INTO `wp_terms` (`name`, `slug`, `term_group`)
+VALUES
+	('Winter', 'winter', 0);
+
+INSERT INTO `wp_term_taxonomy` (`term_id`, `taxonomy`, `description`, `parent`, `count`)
+VALUES
+	(LAST_INSERT_ID(), 'notification-icon', '', 0, 0);
