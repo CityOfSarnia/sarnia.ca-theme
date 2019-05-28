99ede47adf5dea6915a449dde721608aea0a33c0
Zachary Collette <zachary.collette@sarnia.ca>
2019-05-28 12:35:30 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1063,7 +1063,7 @@ add_theme_support('soil-clean-up');
 add_theme_support('soil-disable-rest-api');
 add_theme_support('soil-disable-asset-versioning');
 add_theme_support('soil-disable-trackbacks');
-add_theme_support('soil-google-analytics', env('GOOGLE_ANALYTICS_TRACKINGID'));
+//add_theme_support('soil-google-analytics', env('GOOGLE_ANALYTICS_TRACKINGID'));
 //add_theme_support('soil-js-to-footer');
 add_theme_support('soil-nav-walker');
 add_theme_support('soil-nice-search');
