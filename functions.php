6a190785633748ab879ae3737cad299dc05f601e
Zachary Collette <zachary.collette@sarnia.ca>
2019-05-28 11:57:55 -0400

    disable js to footer function in soil

--- functions.php
+++ functions.php
@@ -1064,7 +1064,7 @@ add_theme_support('soil-disable-rest-api');
 add_theme_support('soil-disable-asset-versioning');
 add_theme_support('soil-disable-trackbacks');
 add_theme_support('soil-google-analytics', env('GOOGLE_ANALYTICS_TRACKINGID'));
-add_theme_support('soil-js-to-footer');
+//add_theme_support('soil-js-to-footer');
 add_theme_support('soil-nav-walker');
 add_theme_support('soil-nice-search');
 add_theme_support('soil-relative-urls');
