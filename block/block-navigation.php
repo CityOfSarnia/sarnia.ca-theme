47f3fd55237952b12caf34af7d851c8b66ff1bfb
Zachary Collette <zachary.collette@sarnia.ca>
2019-05-31 15:44:00 -0400

    modify default behaviour for navigation block.  now populates with child pages.

--- block/block-navigation.php
+++ block/block-navigation.php
@@ -42,6 +42,44 @@
 
 <?php } else { ?>
 
-  <p>Select a menu...</p>
+  <div class="navigation-card <?=card_colour(); ?>">
+
+    <?php if( get_field('menu_image') ) { ?>
+
+      <div class="navigation-card__image" style="background-image: url(<?php $image = get_field('menu_image'); echo($image['sizes']['large']); ?>) !important;"></div>
+
+    <?php } ?>
+
+    <div class="navigation-card__main">
+
+      <?php if( get_field('menu_headline') ) { ?>
+
+        <h2 class="navigation-card__headline"><?php the_field('menu_headline');?></h2>
+
+      <?php } else { ?>
+
+        <h2 class="navigation-card__headline"><?php echo "Navigation" ?></h2>
+
+      <?php } ?>
+
+      <div class="navigation-card__menu">
+
+        <?php
+
+          $children = wp_list_pages( array(
+            'title_li' => '',
+            'child_of' => my_acf_post_id(),
+            'echo'     => 0
+          ) );
+
+          if ( $children ) echo $children;
+
+        ?>
+
+      </div>
+
+    </div>
+
+  </div>
 
 <?php } ?>
