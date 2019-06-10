a649fb9ea9bc3d4e83124aa0a00db9a68e8343d9
Zachary Collette <zachary.collette@sarnia.ca>
2019-06-10 12:19:08 -0400

    add theme support for soil-js-to-footer

--- functions.php
+++ functions.php
@@ -1102,7 +1102,7 @@ add_theme_support('soil-clean-up');
 //add_theme_support('soil-disable-rest-api');
 add_theme_support('soil-disable-asset-versioning');
 add_theme_support('soil-disable-trackbacks');
-//add_theme_support('soil-js-to-footer');
+add_theme_support('soil-js-to-footer');
 add_theme_support('soil-nav-walker');
 add_theme_support('soil-nice-search');
 add_theme_support('soil-relative-urls');
