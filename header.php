20b7272f9c4d78434d2213a17fd94d4f64b15af3
Jonathan Bennett <jonathan@jbennett.me>
2019-07-16 12:12:48 -0400

    Remove Google CSE javascript

--- header.php
+++ header.php
@@ -79,21 +79,6 @@
 
 			<a href="/" class="logo">City of Sarnia</a>
 
-			<div class="search-form">
-				<script>
-					(function() {
-						var cx = '<?=env('GOOGLE_CSE_CX')?>';
-						var gcse = document.createElement('script');
-						gcse.type = 'text/javascript';
-						gcse.async = true;
-						gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-						var s = document.getElementsByTagName('script')[0];
-						s.parentNode.insertBefore(gcse, s);
-					})();
-				</script>
-				<gcse:searchbox-only resultsUrl="/search"></gcse:searchbox-only>
-			</div>
-
 		</div>
 	</header>
 
--- search.php
+++ search.php
@@ -14,21 +14,6 @@ Template Name: Search
 
 		<div class="container container--md">
 			<?php the_content(); ?>
-
-			<div class="search-results">
-				<script>
-					(function() {
-						var cx = '<?=env('GOOGLE_CSE_CX')?>';
-						var gcse = document.createElement('script');
-						gcse.type = 'text/javascript';
-						gcse.async = true;
-						gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-						var s = document.getElementsByTagName('script')[0];
-						s.parentNode.insertBefore(gcse, s);
-					})();
-				</script>
-				<gcse:searchresults-only></gcse:searchresults-only>
-			</div>
 		</div>
 	</article>
 
