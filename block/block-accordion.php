87897bdf2c01e9a8e2b85dce0733d08b30206e19
Zachary Collette <zachary.collette@sarnia.ca>
2019-07-23 13:47:55 -0400

    Add Accordion Heading Settings to Accordion Block This grants the ability to for users to change the Heading Level of Accordions

--- block/block-accordion.php
+++ block/block-accordion.php
@@ -6,13 +6,13 @@
  */
 ?>
 
-<?php if( get_field('accordion_headline') &&  get_field('accordion_content') ) { ?>
+<?php if( get_field('accordion_heading_text') &&  get_field('accordion_content') ) { ?>
 
   <article class="accordion js-accordion__toggle">
 
     <header class="accordion__header">
 
-      <h2 class="accordion__headline"><?php the_field('accordion_headline'); ?></h2>
+      <<?php the_field('accordion_heading_level'); ?> class="accordion__headline"><?php the_field('accordion_heading_text'); ?></<?php the_field('accordion_level'); ?>>
 
       <div class="accordion__toggle"></div>
 
