a8fedc016f81b6ce1d26a4602bf6f83f81628c61
Zachary Collette <zcollette@zcconsulting.ca>
2019-08-11 20:17:30 -0400

    use alternate structure for while

--- search.php
+++ search.php
@@ -1,31 +1,30 @@
 <?php
 /*
-Template Name: Search
-*/
+ * Template Name: Search
+ */
 ?>
 
-<?php include(TEMPLATEPATH . '/header.php'); ?>
+<?php include TEMPLATEPATH . '/header.php';?>
 
-<?php get_header(); ?>
+<?php get_header();?>
 
 <?php if (have_posts() && get_search_query()): ?>
 
-	<article>
-		<div class="container container--md">
-			<div class="search-results">
-				<?php while (have_posts()) {
-					the_post();
-					get_template_part( 'partials/content/content', 'excerpt' );
-				}
-		                sarnia_number_pagination();
-				?>
-			</div>
-		</div>
-	</article>
+<article>
+    <div class="container container--md">
+        <div class="search-results">
+            <?php while (have_posts()): ?>
+            <?php the_post();?>
+            <?php get_template_part('partials/content/content', 'excerpt');?>
+            <?php endwhile;?>
+            <?php sarnia_number_pagination();?>
+        </div>
+    </div>
+</article>
 <?php else: ?>
-	<?php get_template_part( 'partials/content/content', 'none' ); ?>
-<?php endif ?>
+<?php get_template_part('partials/content/content', 'none');?>
+<?php endif?>
 
-<?php get_sidebar(); ?>
+<?php get_sidebar();?>
 
-<?php get_footer(); ?>
+<?php get_footer();?>
