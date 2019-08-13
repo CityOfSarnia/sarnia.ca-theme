fc6b4a3040c2733b745f3e7fb1b27e5a1746f370
Zachary Collette <zachary.collette@sarnia.ca>
2019-08-13 14:57:16 -0400

    change from using direct include to using get_template_part()

--- footer.php
+++ footer.php
@@ -48,23 +48,23 @@
 
 					<div class="footer-social">
 						<?php if( get_field('twitter', 'option') ) { ?>
-						<a href="<?php the_field('twitter', 'option'); ?>" class="social-links__icon" aria-label="Visit Twitter"><?php include(TEMPLATEPATH . '/assets/img/social-twitter-icon.svg'); ?></a>
+						<a href="<?php the_field('twitter', 'option'); ?>" class="social-links__icon" aria-label="Visit Twitter"><?php get_template_part('/assets/img/icons/social/inline', 'twitter.svg'); ?></a>
 						<?php } ?>
 
 						<?php if( get_field('facebook', 'option') ) { ?>
-						<a href="<?php the_field('facebook', 'option'); ?>" class="social-links__icon"  aria-label="Visit Facebook"><?php include(TEMPLATEPATH . '/assets/img/social-facebook-icon.svg'); ?></a>
+						<a href="<?php the_field('facebook', 'option'); ?>" class="social-links__icon"  aria-label="Visit Facebook"><?php get_template_part('/assets/img/icons/social/inline', 'facebook.svg'); ?></a>
 						<?php } ?>
 
 						<?php if( get_field('linkedin', 'option') ) { ?>
-						<a href="<?php the_field('linkedin', 'option'); ?>" class="social-links__icon" aria-label="Visit LinkedIn"><?php include(TEMPLATEPATH . '/assets/img/social-linkedin-icon.svg'); ?></a>
+						<a href="<?php the_field('linkedin', 'option'); ?>" class="social-links__icon" aria-label="Visit LinkedIn"><?php get_template_part('/assets/img/icons/social/inline', 'linkedin.svg'); ?></a>
 						<?php } ?>
 
 						<?php if( get_field('youtube', 'option') ) { ?>
-						<a href="<?php the_field('youtube', 'option'); ?>" class="social-links__icon" aria-label="Visit Youtube"><?php include(TEMPLATEPATH . '/assets/img/social-youtube-icon.svg'); ?></a>
+						<a href="<?php the_field('youtube', 'option'); ?>" class="social-links__icon" aria-label="Visit Youtube"><?php get_template_part('/assets/img/icons/social/inline', 'youtube.svg'); ?></a>
 						<?php } ?>
 
 						<?php if( get_field('instagram', 'option') ) { ?>
-						<a href="<?php the_field('instagram', 'option'); ?>" class="social-links__icon" aria-label="Visit Instagram"><?php include(TEMPLATEPATH . '/assets/img/social-instagram-icon.svg'); ?></a>
+						<a href="<?php the_field('instagram', 'option'); ?>" class="social-links__icon" aria-label="Visit Instagram"><?php get_template_part('/assets/img/icons/social/inline', 'instagram.svg'); ?></a>
 						<?php } ?>
 					</div>
 
