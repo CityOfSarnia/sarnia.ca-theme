148886505171a00e8a82ef7c03338714d829047c
Zachary Collette <zcollette@zcconsulting.ca>
2019-08-16 20:29:14 -0400

    move partials to template-parts

--- search.php
+++ search.php
@@ -9,14 +9,14 @@
         <div class="search-results">
             <?php while (have_posts()): ?>
             <?php the_post();?>
-            <?php get_template_part('partials/content/content', 'excerpt');?>
+            <?php get_template_part('template-parts/content/content', 'excerpt');?>
             <?php endwhile;?>
             <?php sarnia_number_pagination();?>
         </div>
     </div>
 </article>
 <?php else: ?>
-<?php get_template_part('partials/content/content', 'none');?>
+<?php get_template_part('template-parts/content/content', 'none');?>
 <?php endif?>
 
 <?php get_sidebar();?>
