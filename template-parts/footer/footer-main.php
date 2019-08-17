388e10976aefa9c0f1dee4b69bba88e00c7fbb47
Zachary Collette <zcollette@zcconsulting.ca>
2019-08-17 10:08:11 -0400

    use variables with get_field, rather than multiple function calls

--- template-parts/footer/footer-main.php
+++ template-parts/footer/footer-main.php
@@ -9,61 +9,59 @@
                 <div class="contact-list">
                     <h2 class="contact-list__headline">Contact the City</h2>
                     <div class="contact-list__item contact-item">
-<?php if( get_field('phone', 'option') ) : ?>
+<?php if ($phone_option = get_field('phone', 'option')): ?>
                         <p class="contact-item__headline">Phone</p>
-                        <p class="contact-item__text"><?php the_field('phone', 'option'); ?></p>
-<?php endif; ?>
+                        <p class="contact-item__text">
+                            <a href="tel:+1-<?=$phone_option;?>"><?=$phone_option?></a>
+<?php endif;?>
                     </div><!-- .contact-list__item contact-item -->
                     <div class="contact-list__item contact-item">
-<?php if( get_field('address', 'option') ) : ?>
+<?php if ($address_option = get_field('address', 'option')): ?>
                         <p class="contact-item__headline">Address</p>
-                        <p class="contact-item__text"><?php the_field('address', 'option'); ?></p>
-<?php endif; ?>
+                        <p class="contact-item__text"><?=$address_option;?></p>
+<?php endif;?>
                     </div><!-- .contact-list__item contact-item -->
                     <div class="contact-list__item contact-item">
-<?php if( get_field('hours', 'option') ) : ?>
+<?php if ($hours_option = get_field('hours', 'option')): ?>
                         <p class="contact-item__headline">Hours</p>
-                        <p class="contact-item__text"><?php the_field('hours', 'option'); ?></p>
-<?php endif; ?>
+                        <p class="contact-item__text"><?=$hours_option;?></p>
+<?php endif;?>
                     </div><!-- .contact-list__item contact-item -->
                     <div class="contact-list__item contact-item">
-<?php if( get_field('email', 'option') ) : ?>
+<?php if ($email_option = get_field('email', 'option')): ?>
                         <p class="contact-item__headline">Email</p>
                         <p class="contact-item__text">
-                            <a href="mailto:<?php the_field('email', 'option'); ?>">
-                                <?php the_field('email', 'option'); ?>
-
-                            </a>
+                            <a href="mailto:<?=$email_option;?>"><?=$email_option;?></a>
                         </p>
-<?php endif; ?>
+<?php endif;?>
                     </div><!-- .contact-list__item contact-item -->
                 </div><!-- .contact-list -->
                 <div class="footer-social">
-<?php if( get_field('twitter', 'option') ) : ?>
-                    <a href="<?php the_field('twitter', 'option'); ?>" class="social-links__icon" aria-label="Visit Twitter">
-<?php get_template_part('/assets/img/icons/social/inline', 'twitter.svg'); ?>
+<?php if ($twitter_option = get_field('twitter', 'option')): ?>
+                    <a href="<?=$twitter_option?>" class="social-links__icon" aria-label="Visit Twitter">
+<?php get_template_part('/assets/img/icons/social/inline', 'twitter.svg');?>
                     </a><!-- .social-links__icon -->
-<?php endif; ?>
-<?php if( get_field('facebook', 'option') ) : ?>
-                    <a href="<?php the_field('facebook', 'option'); ?>" class="social-links__icon" aria-label="Visit Facebook">
-<?php get_template_part('/assets/img/icons/social/inline', 'facebook.svg'); ?>
+<?php endif;?>
+<?php if ($facebook_option = get_field('facebook', 'option')): ?>
+                    <a href="<?=$facebook_option;?>" class="social-links__icon" aria-label="Visit Facebook">
+<?php get_template_part('/assets/img/icons/social/inline', 'facebook.svg');?>
                     </a><!-- .social-links__icon -->
-<?php endif; ?>
-<?php if( get_field('linkedin', 'option') ) : ?>
-                    <a href="<?php the_field('linkedin', 'option'); ?>" class="social-links__icon" aria-label="Visit LinkedIn">
-<?php get_template_part('/assets/img/icons/social/inline', 'linkedin.svg'); ?>
+<?php endif;?>
+<?php if ($linkedin_option = get_field('linkedin', 'option')): ?>
+                    <a href="<?=$linkedin_option;?>" class="social-links__icon" aria-label="Visit LinkedIn">
+<?php get_template_part('/assets/img/icons/social/inline', 'linkedin.svg');?>
                     </a><!-- .social-links__icon -->
-<?php endif; ?>
-<?php if( get_field('youtube', 'option') ) : ?>
-                    <a href="<?php the_field('youtube', 'option'); ?>" class="social-links__icon" aria-label="Visit Youtube">
-<?php get_template_part('/assets/img/icons/social/inline', 'youtube.svg'); ?>
+<?php endif;?>
+<?php if ($youtube_option = get_field('youtube', 'option')): ?>
+                    <a href="<?=$youtube_option;?>" class="social-links__icon" aria-label="Visit Youtube">
+<?php get_template_part('/assets/img/icons/social/inline', 'youtube.svg');?>
                     </a><!-- .social-links__icon -->
-<?php endif; ?>
-<?php if( get_field('instagram', 'option') ) : ?>
-                    <a href="<?php the_field('instagram', 'option'); ?>" class="social-links__icon" aria-label="Visit Instagram">
-<?php get_template_part('/assets/img/icons/social/inline', 'instagram.svg'); ?>
+<?php endif;?>
+<?php if ($instagram_option = get_field('instagram', 'option')): ?>
+                    <a href="<?=$instagram_option;?>" class="social-links__icon" aria-label="Visit Instagram">
+<?php get_template_part('/assets/img/icons/social/inline', 'instagram.svg');?>
                     </a><!-- .social-links__icon -->
-<?php endif; ?>
+<?php endif;?>
                 </div><!-- .footer-social -->
             </div><!-- .footer__contact -->
         </div><!-- .footer__columns -->
