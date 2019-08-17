1e5435f143f90557dce396f23e4c0f4f5a2ab858
Zachary Collette <zcollette@zcconsulting.ca>
2019-08-17 12:52:40 -0400

    rename template-parts for footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		</main>
 		<footer id="colophon" class="footer" role="contentinfo">
-<?php get_template_part('template-parts/footer/footer', 'main'); ?>
-<?php get_template_part('template-parts/footer/footer', 'menu'); ?>
+<?php get_template_part('template-parts/footer/main'); ?>
+<?php get_template_part('template-parts/footer/navigation'); ?>
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
 <?php wp_footer(); ?>
