ff602924e4753fd4a7604217fbeb5b67a3eefcc2
Zachary Collette <zcollette@zcconsulting.ca>
2019-08-17 10:28:50 -0400

    code indentation for clarity, whitespace removal

--- footer.php
+++ footer.php
@@ -1,16 +1,9 @@
-</main>
-
-<footer id="colophon" class="footer" role="contentinfo">
-
-	<?php get_template_part('template-parts/footer/footer', 'main'); ?>
-	<?php get_template_part('template-parts/footer/footer', 'menu'); ?>
-
-</footer><!-- #colophon -->
-
-</div><!-- #page -->
-
+		</main>
+		<footer id="colophon" class="footer" role="contentinfo">
+<?php get_template_part('template-parts/footer/footer', 'main'); ?>
+<?php get_template_part('template-parts/footer/footer', 'menu'); ?>
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
 <?php wp_footer(); ?>
-
 </body>
-
 </html>
