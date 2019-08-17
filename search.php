9b5e1eaec8c9645015de31035915d3e8d4ac4a03
Zachary Collette <zcollette@zcconsulting.ca>
2019-08-16 20:50:19 -0400

    code formatting/cleanup

--- search.php
+++ search.php
@@ -1,24 +1,22 @@
 <?php /* Template Name: Search */ ?>
 
 <?php get_header();?>
-
 <?php if (have_posts() && get_search_query()): ?>
-
 <article>
     <div class="container container--md">
         <div class="search-results">
-            <?php while (have_posts()): ?>
-            <?php the_post();?>
-            <?php get_template_part('template-parts/content/content', 'excerpt');?>
-            <?php endwhile;?>
-            <?php sarnia_number_pagination();?>
-        </div>
-    </div>
+<?php 
+while (have_posts()):
+    the_post();
+    get_template_part('template-parts/content/content', 'excerpt');
+endwhile;
+sarnia_number_pagination();
+?>
+        </div><!-- .search-results -->
+    </div><!-- .container container--md -->
 </article>
 <?php else: ?>
 <?php get_template_part('template-parts/content/content', 'none');?>
 <?php endif?>
-
 <?php get_sidebar();?>
-
 <?php get_footer();?>
