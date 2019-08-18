d055242aee2f9bcb1e552077debb2890c46c969f
Zachary Collette <zcollette@zcconsulting.ca>
2019-08-18 11:30:29 -0400

    remove redundant page templates

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php get_header(); ?>
-
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-	<article>
-
+	<article id="post-<?php the_ID();?>">
 		<div class="container container--min content">
-
-			<?php the_content(); ?>
-
-		</div>
-
-	</article>
-
+<?php the_content(); ?>
+		</div><!-- .container.container--min.content -->
+	</article><!-- #post-<?php the_ID();?> -->
 <?php endwhile; endif; ?>
-
 <?php get_footer(); ?>
